45635078f5b827b618deef3d29f5813c5c48bf24
Josh Glendenning <git@isobit.io>
2022-02-26 13:03:31 -0600

    No default in help for required vars

--- help.go
+++ help.go
@@ -14,7 +14,7 @@ var helpTemplateString = `USAGE:
 
 OPTIONS:
 {{- range .Flags}}
-\t    \t{{if .ShortName}}-{{.ShortName}}, {{end}}--{{.Name}}{{if .HasArg}} <{{if .Placeholder}}{{.Placeholder}}{{else}}VALUE{{end}}>{{end}}\t{{if and .HasArg .Default}}  (default: {{.Default}}){{else if .Required}}  (required){{end}}\t{{if .Help}}  {{.Help}}{{end}}
+\t    \t{{if .ShortName}}-{{.ShortName}}, {{end}}--{{.Name}}{{if .HasArg}} <{{if .Placeholder}}{{.Placeholder}}{{else}}VALUE{{end}}>{{end}}\t{{if and .HasArg .Default (not .Required)}}  (default: {{.Default}}){{end}}\t{{if .Help}}  {{.Help}}{{end}}
 {{- end}}
 {{- end}}
 {{- if .Commands}}
